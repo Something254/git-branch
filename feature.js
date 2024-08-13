cdc79189f84ac4cdfd502d0244a89fd47051ab67
Someting254 <onyiaorah.zimife@rosevilleschool.org>
2024-08-13 19:39:01 +0100

    features edited

--- feature.js
+++ feature.js
@@ -4,3 +4,16 @@
 // 3. Inside your bug-fix branch write a conditional statement to check if a number is odd or even. commit and push.
 // 4. Inside the feature branch write a conditional statement to find the largest between two numbers. commit and push
 // 5. Create a pull request and merge both branches
+
+
+
+
+const number1 = 50;
+const number2 = 32;
+if(number1 > number2){
+    console.log(number1 + ' is greater than ' + number2);
+} else{
+        console.log(number2 + ' is greater than ' + number1);
+    }
+
+
