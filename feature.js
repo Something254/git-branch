2d1168c44b75e6e006c9d3d75afd3879150ed8ec
Someting254 <onyiaorah.zimife@rosevilleschool.org>
2024-08-13 19:19:14 +0100

    features

---
+++ feature.js
@@ -0,0 +1,6 @@
+// 1.Create a repo called git-branch.{DONE}
+// Create two branches named bug-fix and feature{DONE}
+// 2. Commit and push to github
+// 3. Inside your bug-fix branch write a conditional statement to check if a number is odd or even. commit and push.
+// 4. Inside the feature branch write a conditional statement to find the largest between two numbers. commit and push
+// 5. Create a pull request and merge both branches
