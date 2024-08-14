9bed8cce6612368a14e025115908b092b189c937
Someting254 <onyiaorah.zimife@rosevilleschool.org>
2024-08-14 10:27:19 +0100

    edited

--- feature.js
+++ feature.js
@@ -6,9 +6,9 @@
 const number1 = 50;
 const number2 = 32;
 if(number1 > number2){
-    console.log(number1 + ' is greater than ' + number2);
+    console.log(`${number1} is greater than ${number2}`);
 } else{
-        console.log(number2 + ' is greater than ' + number1);
+    console.log(`${number2} is greater than ${number1}`);
     }
 
 
@@ -22,4 +22,4 @@ const largest_number = Math.max(...numbers);
 
 
 console.log('Largest Value:', largest_number);
-console.log('Smallest Value:', smallest_number); 
+console.log('Smallest Value:', smallest_number);
